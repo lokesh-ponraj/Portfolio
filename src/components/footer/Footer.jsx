38c4ca84a70afe831be58b49d4f901cc99586a02
lokesh-ponraj <tech.lokeshponraj@gmail.com>
2026-07-21 21:53:08 +0530

    Update LinkedIn link in footer to point to the correct URL

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
                 <div className="footer__social">
                     <a href="https://www.github.com/lokesh-ponraj" className="footer__social-link" target="_blank" rel='noreferrer' aria-label="GitHub"><i className="bx bxl-github"></i></a>
                     {/* TODO: swap in the real LinkedIn profile URL */}
-                    <a href="#" className="footer__social-link" target="_blank" rel='noreferrer' aria-label="LinkedIn"><i className="bx bxl-linkedin"></i></a>
+                    <a href="/" className="footer__social-link" target="_blank" rel='noreferrer' aria-label="LinkedIn"><i className="bx bxl-linkedin"></i></a>
                     <a href="mailto:tech.lokeshponraj@gmail.com" className="footer__social-link" aria-label="Email"><i className="bx bx-mail-send"></i></a>
                 </div>
 
